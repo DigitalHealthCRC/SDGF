d8b6781f4888f37ac4500f1915d82dfa889f7680
Amir <amir.marashi@gmail.com>
2025-11-05 15:02:28 +1100

    Remove Storybook tooling and assets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 
-export default [...nextCoreWebVitals, {
-  ignores: ['node_modules/**', '.next/**'],
-}, ...storybook.configs["flat/recommended"]];
+export default [
+  ...nextCoreWebVitals,
+  {
+    ignores: ['node_modules/**', '.next/**'],
+  },
+];
 
